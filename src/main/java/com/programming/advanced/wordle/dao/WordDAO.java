ae4f14c85a7fe33645b0b6b5929d5c26626df6f9
inukaki <inukaki8@gmail.com>
2025-05-20 11:52:36 +0900

    指定したIDの単語を所得するメソッドを実装

--- src/main/java/com/programming/advanced/wordle/dao/WordDAO.java
+++ src/main/java/com/programming/advanced/wordle/dao/WordDAO.java
@@ -32,4 +32,34 @@ public class WordDAO {
             throw new SQLException("単語が見つかりません");
         }
     }
+
+    /**
+     * 指定されたIDの単語を取得します
+     * @param id 取得する単語のID
+     * @return 指定されたIDの単語
+     * @throws SQLException データベースアクセスエラーが発生した場合
+     */
+    public Word getWordById(int id) throws SQLException {
+        String sql = "SELECT * FROM words WHERE id = ?";
+        
+        try (Connection conn = DatabaseInitializer.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setInt(1, id);
+            
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return new Word(
+                        rs.getInt("id"),
+                        rs.getString("word"),
+                        rs.getInt("playCount"),
+                        rs.getInt("clearCount"),
+                        rs.getInt("missCount")
+                    );
+                }
+                
+                throw new SQLException("ID=" + id + "の単語が見つかりません");
+            }
+        }
+    }
 }
