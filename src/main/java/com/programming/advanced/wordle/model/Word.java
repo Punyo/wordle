fcd71ab1b7f976ac23cbfc39bf3471a8fa6b4803
Punyo <49717092+Punyo@users.noreply.github.com>
2025-05-25 03:15:23 +0900

    feat: Wordに正規化済みの言葉のフィールドを追加

--- src/main/java/com/programming/advanced/wordle/model/Word.java
+++ src/main/java/com/programming/advanced/wordle/model/Word.java
@@ -4,6 +4,7 @@ package com.programming.advanced.wordle.model;
 public class Word {
     private int id; // 単語ID
     private String word; // 単語本体（5文字）
+    private String normalizedWord; // 正規化された単語（小文字除去）
     private int playCount; // 出題された回数
     private int clearCount; // 正解された回数
     private int missCount; // 失敗された回数
@@ -11,9 +12,10 @@ public class Word {
     public Word() {
     }
 
-    public Word(int id, String word, int playCount, int clearCount, int missCount) {
+    public Word(int id, String word, String normalizedWord, int playCount, int clearCount, int missCount) {
         this.id = id;
         this.word = word;
+        this.normalizedWord = normalizedWord;
         this.playCount = playCount;
         this.clearCount = clearCount;
         this.missCount = missCount;
@@ -29,6 +31,10 @@ public class Word {
 
     public String getWord() {
         return word;
+    }
+
+    public String getNormalizedWord() {
+        return normalizedWord;
     }
 
     public void setWord(String word) {
